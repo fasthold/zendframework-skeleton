a48922043f33016d8978f0ce1d3f2993d7d5e5f2
Fasthold <fasthold@gmail.com>
2013-03-01 14:49:00 +0800

    Added database manipulation example

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,11 +3,19 @@
 /**
 * IndexController
 */
-class IndexController extends Zend_Controller_Action
+class IndexController extends Base_Controller_Action
 {
 	
+	/**
+	 * IndexAction
+	 */
 	public function indexAction()
 	{
-		
+		// Database manipulation example:
+		/*
+		$db = Zend_Registry::get(REGKEY_DATABASE);
+		$result = $db->fetchAll('select 1+1');
+		debug($result);
+		*/
 	}
 }
